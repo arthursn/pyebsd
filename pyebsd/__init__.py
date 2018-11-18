e8d8f07741d3e3ee3b82d6dd2deafeb302acb2ed
Arthur Nishikawa <nishikawa.poli@gmail.com>
2018-11-18 13:19:32 +0100

    __init__.py changed to be compatible with new packages structure

--- pyebsd/__init__.py
+++ pyebsd/__init__.py
@@ -1,10 +1,8 @@
 from .ebsd import *
+from .io import *
 from .crystal import *
 from .selection import *
 from .draw import *
 from .misc import *
-
-import os
-DIR = os.path.dirname(os.path.abspath(__file__))
 
 from .__version import __version__
--- pyebsd/draw/__init__.py
+++ pyebsd/draw/__init__.py
@@ -2,4 +2,3 @@ from .display import *
 from .pole_figure import *
 from .pilutil import *
 from .scalebar import ScaleBar
-from .progressbar import ProgressBar
