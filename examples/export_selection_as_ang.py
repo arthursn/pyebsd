f025730c79ae23cd39d353ebce54e8799cd0c47e
Arthur Nishikawa <nishikawa.poli@gmail.com>
2018-11-19 17:08:36 +0100

    Change name of exported ang file to selection.ang

--- examples/export_selection_as_ang.py
+++ examples/export_selection_as_ang.py
@@ -25,11 +25,11 @@ plt.ioff()
 # plot ipf of the selection
 ipfsel = scan.plot_IPF(sel=ipf.sel, gray=scan.IQ)
 
-# save selection as ADI_selection.ang
-pyebsd.save_ang_file('ADI_selection.ang', scan, ipf.sel)
+# save selection as selection.ang
+pyebsd.save_ang_file('selection.ang', scan, ipf.sel)
 
-# load ADI_selection.ang
-scansel = pyebsd.load_scandata('ADI_selection.ang')
+# load selection.ang
+scansel = pyebsd.load_scandata('selection.ang')
 # plot ipf from selection
 scansel = scansel.plot_IPF(gray=scansel.IQ)
 
