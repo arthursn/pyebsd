95347ade0779604382b0bc370c361a465302171c
Arthur Nishikawa <nishikawa.poli@gmail.com>
2020-03-04 18:50:51 +0100

    Plotting enhanced; Prints ji coordinates according to pyebsd convention

--- development_tests/test_get_neighbor_pixels.py
+++ development_tests/test_get_neighbor_pixels.py
@@ -11,40 +11,56 @@ def do_n_rotations(R, p, n):
 
 
 if __name__ == '__main__':
-    nturns = 6
-    theta = 2.*np.pi/nturns
-    R = [[np.cos(theta), -np.sin(theta)],
-         [np.sin(theta), np.cos(theta)]]
+    c60 = np.cos(np.pi/3.)
+    s60 = np.sin(np.pi/3.)
+    R = [[c60, -s60],
+         [s60, c60]]
+
     A = [[1., .5],
          [0., .5*3.**.5]]
+
+    B = [[.5, -.5],
+         [0, 1]]
 
     x_mult6 = []
     y_mult6 = []
     x_mult12 = []
     y_mult12 = []
     neighbors = []
-    n = 10
+    n = 6
     for ijsum in range(n+1):
         for i in range(1, ijsum+1):
             j = ijsum - i
             if j <= i:
-                ijneighbor = []
+                xyneighbor = []
+                jineighbor = []
 
-                print(i, j, ijsum, i**2 + j**2 + i*j)
 
                 p = np.dot(A, [i, j])
-                ijneighbor = do_n_rotations(R, p, 5)
+                xyneighbor = do_n_rotations(R, p, 5)
 
                 if (j != 0 and i != j):
                     p = np.dot(A, [j, i])
                     # https://codegolf.stackexchange.com/questions/169893/python-shortest-way-to-interleave-items-from-two-lists
                     # Interleave lists
-                    ijneighbor = [*sum(zip(ijneighbor, do_n_rotations(R, p, 5)), ())]
+                    xyneighbor = [*sum(zip(xyneighbor, do_n_rotations(R, p, 5)), ())]
 
-                neighbors.append(ijneighbor)
+                for x, y in xyneighbor:
+                    jineighbor.append([(x/c60).round(0).astype(int), (y/s60).round(0).astype(int)])
 
-    for k, ijneighbor in enumerate(neighbors):
-        plt.plot(*np.array(ijneighbor).T, marker='.', lw=.5, label=str(k))
+                print(i, j, ijsum, i**2 + j**2 + i*j, jineighbor)
+                # for r in range(1, 6):
+                #     xy = np.dot(R60, xy)  # 60 degrees rotation
+
+                neighbors.append(xyneighbor)
+
+    th = np.linspace(0, 2*np.pi, 100)
+    cth = np.cos(th)
+    sth = np.sin(th)
+    for k, xyneighbor in enumerate(neighbors):
+        r = (xyneighbor[0][0]**2. + xyneighbor[0][1]**2.)**.5
+        pts, = plt.plot(*np.array(xyneighbor).T, marker='.', ls='none', label='{:.3g}'.format(r))
+        plt.plot(r*cth, r*sth, color=pts.get_color(), lw=.5)
 
     plt.gca().set_aspect('equal')
     plt.legend(ncol=2)
