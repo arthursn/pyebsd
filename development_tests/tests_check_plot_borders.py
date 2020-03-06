b75f8df12203654d0c879cec05a4b1be6e6c779f
Arthur Nishikawa <nishikawa.poli@gmail.com>
2020-03-07 00:03:00 +0100

    Code cleaned up

--- development_tests/tests_check_plot_borders.py
+++ development_tests/tests_check_plot_borders.py
@@ -2,6 +2,7 @@
 Plotting EBSD maps
 """
 
+from builtins import input
 import matplotlib.pyplot as plt
 import pyebsd
 
@@ -12,37 +13,37 @@ import os
 angfile = os.path.join('..', 'data', 'ADI_bcc_fcc.ang')
 
 # load ebsd scandata
-# So far, pyebsd only supports loading .ang files generated
-# by the TSL OIM software
 scan = pyebsd.load_scandata(angfile)
 
 # Plot inverse pole figure map
-# gray is used to set the quality index as grayscale
 ipfmap = scan.plot_IPF(gray=scan.IQ)
+ipfmap.ax.plot(scan.x, scan.y, 'r.', ms='1')
 
 # Plot IPF map for only the fcc phase (ph == 2)
 ipfmapfcc = scan.plot_IPF(sel=(scan.ph == 2), gray=scan.IQ, tiling='hex')
+ipfmapfcc.ax.plot(scan.x[scan.ph == 2], scan.y[scan.ph == 2], 'r.', ms='1')
 
 # Plot phase map
 phmap = scan.plot_phase(gray=scan.IQ)
-
-ipfmap.ax.plot(scan.x, scan.y, 'r.', ms='1')
-ipfmapfcc.ax.plot(scan.x, scan.y, 'r.', ms='1')
 phmap.ax.plot(scan.x, scan.y, 'k.', ms='1')
+
+# Show plots
 plt.ion()
 plt.show()
 
 phmap.lasso_selector()
+# phmap.rect_selector()
 
 key = input('Type ENTER')
 
 p1 = scan.plot_IPF(sel=(scan.ph == 2) & phmap.sel, gray=scan.IQ, tiling='hex')
-p2 = scan.plot_IPF(sel=(scan.ph == 2) & phmap.sel, gray=scan.IQ, tiling='rect')
-p3 = scan.plot_phase(sel=phmap.sel, gray=scan.IQ, tiling='hex')
-p4 = scan.plot_phase(sel=phmap.sel, gray=scan.IQ, tiling='rect')
 p1.ax.plot(scan.x[(scan.ph == 2) & phmap.sel], scan.y[(scan.ph == 2) & phmap.sel], 'r.', ms='1')
-p2.ax.plot(scan.x[(scan.ph == 2) & phmap.sel], scan.y[(scan.ph == 2) & phmap.sel], 'r.', ms='1')
-p3.ax.plot(scan.x[phmap.sel], scan.y[phmap.sel], 'k.', ms='1')
-p4.ax.plot(scan.x[phmap.sel], scan.y[phmap.sel], 'k.', ms='1')
 
-# Show plots
+p2 = scan.plot_IPF(sel=(scan.ph == 2) & phmap.sel, gray=scan.IQ, tiling='rect')
+p2.ax.plot(scan.x[(scan.ph == 2) & phmap.sel], scan.y[(scan.ph == 2) & phmap.sel], 'r.', ms='1')
+
+p3 = scan.plot_phase(sel=phmap.sel, gray=scan.IQ, tiling='hex')
+p3.ax.plot(scan.x[phmap.sel], scan.y[phmap.sel], 'k.', ms='1')
+
+p4 = scan.plot_phase(sel=phmap.sel, gray=scan.IQ, tiling='rect')
+p4.ax.plot(scan.x[phmap.sel], scan.y[phmap.sel], 'k.', ms='1')
