22f587b07583e6ce46f7fecf9ba816498f985a3c
Arthur Nishikawa <nishikawa.poli@gmail.com>
2020-05-07 01:14:08 +0200

    Added test for extracting IPF directions information from IPF map image file

---
+++ development_tests/get_directions_from_IPF_map.py
@@ -0,0 +1,95 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pyebsd
+from pyebsd.ebsd.plotting import _CoordsFormatter
+from PIL import Image
+
+
+def get_gray_from_color(rgb):
+    if isinstance(rgb, (list, tuple)):
+        rgb = np.array(rgb)
+
+    shape = rgb.shape
+    ndim = rgb.ndim
+    if ndim != 2:
+        rgb = rgb.reshape(-1, 3)
+
+    gray = rgb.max(axis=1)/255
+
+    if ndim != 2:
+        gray = gray.reshape(shape[:-1])
+
+    return gray
+
+
+def get_IPF_from_color(rgb, **kwargs):
+    """
+    Get the IPF direction(s) from the corresponding list color
+    """
+    if isinstance(rgb, (list, tuple)):
+        rgb = np.array(rgb)
+
+    shape = rgb.shape
+    ndim = rgb.ndim
+    if ndim != 2:
+        rgb = rgb.reshape(-1, 3)
+
+    # whitespot: white spot in the unit triangle
+    # By default, whitespot is in the barycenter of the unit triangle
+    whitespot = kwargs.pop('whitespot', [0.48846011, 0.22903335, 0.84199195])
+    pwr = kwargs.pop('pwr', .75)
+
+    # Select variant where w >= u >= v
+    whitespot = np.sort(whitespot)
+    whitespot = whitespot[[1, 0, 2]]
+
+    kR = whitespot[2] - whitespot[0]
+    kG = whitespot[0] - whitespot[1]
+    kB = whitespot[1]
+
+    v = kB*rgb[:, 2]**(1./pwr)
+    u = kG*rgb[:, 1]**(1./pwr) + v
+    w = kR*rgb[:, 0]**(1./pwr) + u
+
+    uvw = np.array([u, v, w])
+    uvwnorm = np.linalg.norm(uvw, axis=0)
+    uvw = (uvw/uvwnorm).T
+
+    if ndim != 2:
+        uvw = uvw.reshape(shape)
+
+    return uvw
+
+
+if __name__ == '__main__':
+    # img = Image.open('../data/unit_triangle.png')
+    img = Image.open('IPF_ND_gray_IQ.png')
+    w, h = img.size
+
+    rgb = np.array(img)[:, :, :3]  # drop alpha channel
+
+    fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(14, 4.2))
+
+    ax1.imshow(rgb)
+    ax1.axis('off')
+    ax1.set_title('Original figure')
+
+    uvw = get_IPF_from_color(rgb)
+    gray = get_gray_from_color(rgb)
+
+    rgb1 = pyebsd.get_color_IPF(uvw)
+    rgb1 = (rgb1.T*gray.T).T.astype(np.uint8)
+
+    ax2.imshow(rgb1)
+    ax2.format_coord = _CoordsFormatter((0, w, 0, h), uvw, 'd')
+    ax2.axis('off')
+    ax2.set_title('Reconstructed figure by first calculating\ndirections and then converting to colors')
+
+    diffmap = ax3.imshow((((rgb.astype(int) - rgb1.astype(int))**2.).sum(axis=2))**.5, vmin=0, vmax=2, cmap='magma')
+    ax3.axis('off')
+    ax3.set_title('Difference (squared residuals)')
+    cbar = fig.colorbar(diffmap, ax=ax3, extend='both')
+
+    fig.tight_layout()
+
+    plt.show()
