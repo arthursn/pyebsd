c834ed917583e2453df2d3af56e66777c1bba35b
Arthur Nishikawa <nishikawa.poli@gmail.com>
2021-03-10 05:07:30 +0100

    Added existing examples as jupyter notebooks

--- examples/plot_unit_triangle.py
+++ examples/plot_unit_triangle.py
@@ -1,39 +1,37 @@
-import numpy as np
 import matplotlib.pyplot as plt
 import pyebsd
 
+fig, axes = plt.subplots(2, 2, figsize=(10, 10))
+ax1, ax2, ax3, ax4 = axes.ravel()
 
-fig, ax = plt.subplots()
-pyebsd.unit_triangle(ax=ax)
-ax.text(0, 1, ('Default color palette\n'
-               'White spot in the barycenter of the unit triangle'),
-        transform=ax.transAxes)
+pyebsd.unit_triangle(ax=ax1)
+ax1.text(0, 1, ('Default color palette\n'
+                'White spot in the barycenter of the unit triangle'),
+         transform=ax1.transAxes)
 
 # --------------------------------------------------
 
 whitespot = [24, 11, 42]
 
-fig, ax = plt.subplots()
-pyebsd.unit_triangle(ax=ax, whitespot=whitespot)
-ax.text(0, 1, 'White spot in the direction {}'.format(whitespot),
-        transform=ax.transAxes)
+pyebsd.unit_triangle(ax=ax2, whitespot=whitespot)
+ax2.text(0, 1, 'White spot in the direction {}'.format(whitespot),
+         transform=ax2.transAxes)
 
 # --------------------------------------------------
 
 whitespot = [48, 20, 83]
 
-fig, ax = plt.subplots()
-pyebsd.unit_triangle(ax=ax, whitespot=whitespot)
-ax.text(0, 1, 'White spot in the direction {}'.format(whitespot),
-        transform=ax.transAxes)
+pyebsd.unit_triangle(ax=ax3, whitespot=whitespot)
+ax3.text(0, 1, 'White spot in the direction {}'.format(whitespot),
+         transform=ax3.transAxes)
 
 # --------------------------------------------------
 
 whitespot = [2, 1, 3]
 
-fig, ax = plt.subplots()
-pyebsd.unit_triangle(ax=ax, whitespot=whitespot)
-ax.text(0, 1, 'White spot in the direction {}'.format(whitespot),
-        transform=ax.transAxes)
+pyebsd.unit_triangle(ax=ax4, whitespot=whitespot)
+ax4.text(0, 1, 'White spot in the direction {}'.format(whitespot),
+         transform=ax4.transAxes)
 
+fig.set_tight_layout('tight')
 plt.show()
