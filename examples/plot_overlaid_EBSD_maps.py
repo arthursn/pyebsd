ca1019db6f3ed75212d187426ae2fd89423a638b
Arthur Nishikawa <nishikawa.poli@gmail.com>
2021-09-20 12:02:51 +0200

    Code refactored

--- examples/plot_overlaid_EBSD_maps.py
+++ examples/plot_overlaid_EBSD_maps.py
@@ -18,11 +18,11 @@ scan = pyebsd.load_scandata(angfile)
 
 # Plot image quality (IQ) map using a gray color palette.
 # colorbar=False is used to hide the colorbar
-ipfmap = scan.plot_property(scan.IQ, cmap='gray', colorbar=False)
+iqmap = scan.plot_property(scan.IQ, cmap='gray', colorbar=False)
 
 # Plot IPF map for only the fcc phase (ph == 2)
 # colorfill='none' sets the background transparent
-ipfmapfcc = scan.plot_IPF(sel=(scan.ph == 2), gray=scan.IQ, colorfill='none', ax=ipfmap.ax)
+ipfmap = scan.plot_IPF(sel=(scan.ph == 2), gray=scan.IQ, colorfill='none', ax=iqmap.ax)
 
 # Show plots
 plt.show()
