276100675119fb3e30bc62c6a8430ba6cba59889
Arthur Nishikawa <nishikawa.poli@gmail.com>
2023-04-03 02:45:18 +0200

    Added pyproject.toml file for setup

--- setup.py
+++ setup.py
@@ -1,21 +1,29 @@
 from setuptools import setup
 
 setup(
-    name='pyebsd',
-    version='0.1dev',
-    description='python library for post-processing of Electron Backscattered Diffraction '
-    '(EBSD) data',
-    author='Arthur Nishikawa',
-    author_email='nishikawa.poli@gmail.com',
-    url='https://github.com/arthursn/pyebsd',
-    packages=['pyebsd', 'pyebsd.ebsd', 'pyebsd.io',
-              'pyebsd.selection', 'pyebsd.draw', 'pyebsd.misc'],
+    name="pyebsd",
+    version="0.1dev",
+    description="python library for post-processing of Electron Backscattered Diffraction "
+    "(EBSD) data",
+    author="Arthur Nishikawa",
+    author_email="nishikawa.poli@gmail.com",
+    url="https://github.com/arthursn/pyebsd",
+    packages=[
+        "pyebsd",
+        "pyebsd.ebsd",
+        "pyebsd.io",
+        "pyebsd.selection",
+        "pyebsd.draw",
+        "pyebsd.misc",
+    ],
     include_package_data=True,
-    install_requires=['matplotlib',
-                      'matplotlib_scalebar',
-                      'numpy',
-                      'pandas',
-                      'pillow',
-                      'scipy'],
-    long_description=open('README.md').read()
+    install_requires=[
+        "matplotlib",
+        "matplotlib_scalebar",
+        "numpy",
+        "pandas",
+        "pillow",
+        "scipy",
+    ],
+    long_description=open("README.md").read(),
 )
